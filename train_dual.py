43683ad07bbbc2a2fb4c95861e6ff0fa43a3ab9b
Tan Pei Yu <120084583+tanpeiyu1209tpy@users.noreply.github.com>
2025-06-23 02:11:18 +0800

    Update train_dual.py

--- train_dual.py
+++ train_dual.py
@@ -410,7 +410,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
         LOGGER.info(f'\n{epoch - start_epoch + 1} epochs completed in {(time.time() - t0) / 3600:.3f} hours.')
         for f in last, best:
             if f.exists():
-                strip_optimizer(f)  # strip optimizers
+                #strip_optimizer(f)  # strip optimizers
                 if f is best:
                     LOGGER.info(f'\nValidating {f}...')
                     results, _, _ = validate.run(
